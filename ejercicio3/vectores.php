95bcc348aa4502edb48348dd5f6a16c1a1796034
blaszla <blassalvador03@gmail.com>
2022-09-26 12:54:41 -0400

    Declaración de vectores

---
+++ ejercicio3/vectores.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Vectores</title>
+</head>
+<body>
+	<h1> Introduccion a los vectores, metodo largo </h1>
+	<p> A continuacion escribiremos los vectores de acuerdo al metodo largo </p>
+	<p>
+	<?php
+		/*
+		Este es el metodo largo para la creacion de vectores, donde cada uno de los
+		arreglos esta en una linea diferente, junto con su valor indice correspondiente
+		*/
+	$dia[0] = "domingo";
+	$dia[1] = "lunes";
+	$dia[2] = "martes";
+	$dia[3] = "miercoles";
+	$dia[4] = "jueves";
+	$dia[5] = "viernes";
+	$dia[6] = "sabado";
+	//mostrar el miercoles
+	echo $dia[3];
+	?>
+</p>
+</body>
+</html>

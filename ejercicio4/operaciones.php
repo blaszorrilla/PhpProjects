afabb470a239fa67c65c10cd3d846689574404b3
blaszla <blassalvador03@gmail.com>
2022-09-26 16:00:47 -0400

    Operaciones aritméticas básicas

---
+++ ejercicio4/operaciones.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Operaciones</title>
+</head>
+<body>
+	<h1>Ejemplo de operaciones aritmeticas en PHP</h1>
+
+	<?php
+	$a = 8; //declaramos un numero entero positivo
+	$b = 3; //declaramos otro numero entero positivo
+	echo "Los numeros son: $a y $b","<br>";
+	echo "La suma es: ", $a + $b, "<br>";
+	echo "La resta es: ", $a - $b, "<br>";
+	echo "La multiplicación es: ", $a * $b, "<br>";
+	echo "La división es: ", $a / $b, "<br>";
+	?>
+</body>
+</html>

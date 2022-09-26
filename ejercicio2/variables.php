0253965cb420d51645626efa5aade42b89d08b7a
blaszla <blassalvador03@gmail.com>
2022-09-26 12:14:02 -0400

    Uso de variables en php

---
+++ ejercicio2/variables.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Variables</title>
+</head>
+<body>
+	<h1>Ejemplo de variables</h1>
+		<p>
+			<?php
+			$nombre = "Juan";
+			echo "Hola <b>$nombre</b>, encantado de conocerte";
+			?>
+		</p>
+</body>
+</html>

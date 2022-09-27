77a7facf328932f51739d2bcc8776d2f349519ba
blaszla <blassalvador03@gmail.com>
2022-09-27 01:29:31 -0400

    Bucle for

---
+++ ejercicio7/for.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Bucle For</title>
+</head>
+<body>
+	<p>El bucle for resulta muy útil cuando debemosejecutar un bloque de código a condición de que una variable se encuentre
+entre un valor mínimo y otro máximo.</p>
+	<?php
+		for ($num = 1; $num <=5; $num++){
+		echo $num."<br>";
+	}
+	?>
+</body>
+</html>

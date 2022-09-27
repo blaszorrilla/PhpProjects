9a6832cde7dfe664763a7c554e8195dee2f96cc9
blaszla <blassalvador03@gmail.com>
2022-09-27 01:28:54 -0400

    Bucle while

---
+++ ejercicio7/while.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Bucle while</title>
+</head>
+<body>
+	<p>La sentencia while ejecuta un bloque de código mientras se cumpla una
+determinada condición. </p>
+	<?php
+		$num = 1;
+		while ($num < 6) {
+		echo $num;
+		$num++;
+	}
+	?> 
+</body>
+</html>
